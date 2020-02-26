748eb27cc64d69806adee154a1d76dafc9214ef8
pushpendraKh <mr.princekhandelwal20@gmail.com>
2020-02-26 17:05:41 +0530

    Updated Pro Fonts ttf
    1. Added .ttf files under /lib/fonts from `https://fontawesome.com/download` -> Pro for web -> unzip -> webfonts
    2. Added icons.json file (extract from metadata from downloaded pro fonts zip file) into lib/icons/icons.json
    3. run ./tool/update.sh to generate new font_awesome_map.dart which contains all fonts (including free once)

--- tool/update.sh
+++ tool/update.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 
-pushd lib/fonts/
-
-curl -O -L "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/webfonts/fa-brands-400.ttf"
-curl -O -L "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/webfonts/fa-regular-400.ttf"
-curl -O -L "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/webfonts/fa-solid-900.ttf"
-
-popd
-
-curl -o /tmp/icons.json "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/metadata/icons.json"
-
-dart ./tool/generate_icons.dart /tmp/icons.json
+dart ./tool/generate_icons.dart ./lib/icons/icons.json
 dartfmt -w ./lib/font_awesome_map.dart
-
-rm /tmp/icons.json
